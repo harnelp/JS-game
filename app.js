05143beff39a7c338f6a60a0e450aec8210e6b55
Harnel <harnelp@gmail.com>
2024-01-21 23:13:37 -0500

    add juego

---
+++ app.js
@@ -0,0 +1,55 @@
+// generar numero aleatorio
+let numeroSecreto = 0;
+let intentos = 0;
+
+//funcion generica para cambiar textos
+function asignarTextoElementos(elemento,texto){
+    elementoHtml = document.querySelector(elemento);
+    elementoHtml.innerHTML = texto;
+    return;
+}
+function verificarIntento(){
+    let numeroDelUsuario = parseInt(document.getElementById('valor_usuario').value);
+
+    if (numeroDelUsuario === numeroSecreto){
+        asignarTextoElementos('p',`Acertaste el número en ${intentos} ${(intentos === 1) ? 'vez':'veces'}`)
+        document.getElementById('reiniciar').removeAttribute('disabled');
+    }else{
+        if(numeroDelUsuario>numeroSecreto){
+            asignarTextoElementos('p','el numero es menor')
+        }else{
+            asignarTextoElementos('p','el numero es mayor')
+        }
+        intentos++;
+        limpiarCaja();
+    }
+    
+}
+
+function generarNumeroSecreto(){
+    return Math.floor(Math.random()*10)+1;
+}
+
+function limpiarCaja(){
+    let valorCaja = document.querySelector('#valor_usuario').value = '';
+}
+
+function inicializandoJuego(){
+    asignarTextoElementos('h1','Adivina el numero secreto');
+    asignarTextoElementos('p','Indica un numero entre 1 y 10');
+    // generar el numero aleatorio
+    numeroSecreto = generarNumeroSecreto();
+    // inicializar los intentos
+    intentos = 1;
+}
+
+function reiniciarJuego(){
+    // lipiar la caja
+    limpiarCaja();
+    // indicaciones
+    inicializandoJuego();
+    // deshabilitar el boton
+    document.querySelector('#reiniciar').setAttribute('disabled', 'true');
+}
+
+inicializandoJuego();
